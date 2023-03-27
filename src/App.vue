c651bb190e0aec6b7afae3a3fab8f621ceaa78c8
GiuseppeSuanno <suanno.giuseppe@yahoo.it>
2023-03-27 16:47:05 +0200

    Implementazione content vuoto

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,20 @@ export default {
 <template>
   <TheHeader></TheHeader>
 
-  <main>main</main>
+  <main>
+    <div class="content">
+      <h1>Il contenuto va qui!</h1>
+    </div>
+
+    <div class="container-mid">
+      <div class="first-item">
+        <img src="" alt="">
+
+      </div>
+
+    </div>
+
+  </main>
   <TheFooter></TheFooter>
 </template>
 
