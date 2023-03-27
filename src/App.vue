f63f4f3528a56502737b4b326d5e16cc6e4c557c
GiuseppeSuanno <suanno.giuseppe@yahoo.it>
2023-03-27 15:26:09 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import TheHeader from "./components/TheHeader.vue"
+import TheFooter from "./components/TheFooter.vue"
+export default {
+  components: {
+    TheHeader,
+    TheFooter,
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<template>
+  <TheHeader></TheHeader>
+
+  <main>main</main>
+  <TheFooter></TheFooter>
+</template>
+
+<style scoped></style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: 'TheHeader'
+}
+</script>
+
+
+
+<template></template>
+<h1>la pasta</h1>
+
+
+
+<style scoped></style>
